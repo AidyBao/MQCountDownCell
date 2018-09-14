90c7ef40cc5decf3976c5dbb959c24ad7277a907
AidyBao <aidybao555@sina.com>
2018-09-14 16:59:27 +0800

    1.0.0
    UITableViewCell倒计时复用

--- MQCountDownCell/ViewController.swift
+++ MQCountDownCell/ViewController.swift
@@ -9,17 +9,67 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var tableView: UITableView!
+    var dataList: Array<MQTimeModel>        = []
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+       self.tableView.register(UINib.init(nibName:String.init(describing: MQTableViewCell.self), bundle: nil), forCellReuseIdentifier: MQTableViewCell.MQTableViewCellID)
+        self.testData()
+        
+        // 启动倒计时管理
+        ZXCountDownManager.share.start()
+    }
+    
+    func testData() {
+        for _ in 0..<20 {
+            let model = MQTimeModel()
+            model.nextDatetime = 2000000
+            model.systime = 200000
+            self.dataList.append(model)
+        }
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+        
+        
+    }
+    
+    deinit {
+        ZXCountDownManager.share.invalidate()
+        ZXCountDownManager.share.reload()
     }
 
-
 }
+
+extension ViewController: UITableViewDelegate {
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        let cellH: CGFloat = 60.0
+        return cellH
+    }
+}
+
+extension ViewController: UITableViewDataSource {
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        let count = self.dataList.count
+        return count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell: MQTableViewCell = tableView.dequeueReusableCell(withIdentifier: MQTableViewCell.MQTableViewCellID, for: indexPath) as! MQTableViewCell
+        if self.dataList.count > 0 {
+            cell.zx_loadData(self.dataList[indexPath.row],indexPath.row)
+        }
+        cell.zxFCallBack = { model in
+            if model.timeOut {
+                
+            }
+        }
+        return cell
+    }
+}
+
 
