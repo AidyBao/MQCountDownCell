ccd47d4ff82d269409a1a9bf55a43c1386c58012
AidyBao <aidybao555@sina.com>
2018-09-14 11:08:29 +0800

    Initial Commit

---
+++ MQCountDownCell/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  MQCountDownCell
+//
+//  Created by 120v on 2018/9/14.
+//  Copyright © 2018年 MQ. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
